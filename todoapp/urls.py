07bd1fcaea7dcf7d712cf7864c6b423dcd1733c2
nurhatmurathan <nurhatmurathan001@gmail.com>
2023-06-17 16:32:11 +0600

    add Routers to optimize paths

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,15 @@
 from .views import *
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 
+router = routers.SimpleRouter()
+router.register(r'user', UserViewSet)
 
 urlpatterns = [
-    path('api/v1/user/', UserViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('api/v1/user/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('api/v1/', include(router.urls)),
+    # path('api/v1/user/', UserViewSet.as_view({'get': 'list', 'post': 'create'})),
+    # path('api/v1/user/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 
 ]
+
